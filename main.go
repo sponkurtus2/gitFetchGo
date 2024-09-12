b25b7ba3b62b65e5eb9d94989a45b9ed06d40bf7
sponkurtus2 <sponkurtus3@gmail.com>
2024-09-12 09:36:45 -0600

    Limit repos call to 3

--- main.go
+++ main.go
@@ -1,8 +1,9 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
-	"io/ioutil"
+	"io"
 	"log"
 	"net/http"
 )
@@ -13,53 +14,76 @@ import (
 //        "https://api.github.com/users/${user_name}"
 //    );
 
-func main() {
+type ResponseData struct {
+	Results []interface{} `json:"results"`
+}
 
+type RepoData struct {
+	Name string `json:"name"`
+	Url  string `json:"html_url"`
+}
+
+func main() {
 	userName := "sponkurtus2"
 	url := fmt.Sprintf("https://api.github.com/users/%s", userName)
 
 	// var token = "Bearer" + "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"
-	var token = "Bearer" + "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"
+	token := "Bearer" + "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"
 
 	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		log.Println("Error", err)
+	}
 	req.Header.Add("Authorization", token)
 
 	client := &http.Client{}
 	resp, err := client.Do(req)
-
 	if err != nil {
 		log.Println("Error on response: ", err)
 	}
 	defer resp.Body.Close()
 
-	_, err = ioutil.ReadAll(resp.Body)
+	_, err = io.ReadAll(resp.Body)
 	if err != nil {
 		log.Println("Error while reading", err)
 	}
 	// log.Println(string([]byte(body)))
 
 	list_repos(userName, token)
-
 }
 
 func list_repos(userName, token string) {
 	url := fmt.Sprintf("https://api.github.com/users/%s/repos", userName)
 
 	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		log.Println("Error: ", err)
+	}
+
 	req.Header.Add("Authorization", token)
 
 	client := &http.Client{}
-	resp, err := client.Do(req)
 
+	resp, err := client.Do(req)
 	if err != nil {
 		log.Println("Error on response...", err)
 	}
 	defer resp.Body.Close()
 
-	body, err := ioutil.ReadAll(resp.Body)
+	body, err := io.ReadAll(resp.Body)
 	if err != nil {
 		log.Println("Error while reading...", err)
 	}
-	log.Println(string([]byte(body)))
 
+	var repos []RepoData
+	if err := json.Unmarshal(body, &repos); err != nil {
+		log.Println("Error when Unmarshal json", err)
+		return
+	}
+
+	if len(repos) > 3 {
+		repos = repos[:3]
+	}
+
+	log.Println(repos)
 }
